e93a3912e98f1148cd89a5a36f135d860bc88254
Siirabs <63456517+Siirabs@users.noreply.github.com>
2021-04-25 18:46:10 +0300

    cat fixes

--- my-cat.c
+++ my-cat.c
@@ -5,12 +5,16 @@ int main(int argc, char ** argv) {
 	char c[MAX];
 	FILE *file;
 	int i = 1;
+	//Checks if there is only one given argument
+	if(argc == 1) {
+		return 0;
+	}
 	//Loop file by file
 	for (; i<argc; i++) {
 		//Exits if reading the file fails
 		if ((file = fopen(argv[i], "r")) == NULL) {
 			printf("my-cat: cannot open file\n");
-			exit(1);
+			exit(0);
 		}
 		//Read until end of file
 		while ((fgets(c, MAX, file)) != NULL) {
